071319ff94ff7eeb42bf67567938598fc9e5cd12
Michael Chapman <m5l.c5n@gmail.com>
2017-07-07 17:02:33 -0400

    add AccordionMenu component

---
+++ src/components/AccordionMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x align-center">
+      <div class="medium-6 cell">
+        <ul class="vertical menu accordion-menu" data-accordion-menu>
+          <li>
+            <a href="#">Item 1</a>
+            <ul class="menu vertical nested">
+              <li><a href="#">Item 1A</a></li>
+              <li><a href="#">Item 1B</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Item 2</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted () {
+      this.menu = new this.$foundation.AccordionMenu($('.accordion-menu'))
+    },
+    destroyed () {
+      this.menu.destroy()
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .grid-container {
+    margin-top: 2rem;
+  }
+
+</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,6 +4,7 @@
     <abide></abide>
     <dropdown></dropdown>
     <slider></slider>
+    <accordion-menu></accordion-menu>
   </div>
 </template>
 
@@ -12,13 +13,15 @@
   import Abide from '@/components/Abide'
   import Dropdown from '@/components/Dropdown'
   import Slider from '@/components/Slider'
+  import AccordionMenu from '@/components/AccordionMenu'
 
   export default {
     components: {
       'top-bar': TopBar,
       'abide': Abide,
       'dropdown': Dropdown,
-      'slider': Slider
+      'slider': Slider,
+      'accordion-menu': AccordionMenu
     }
   }
 </script>
